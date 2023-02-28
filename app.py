70204975e6bfd4c9d6bdc901e889508b7731e02d
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-02-28 14:35:57 -0600

    V1.5
    Not properly connected, will fiix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import url_for, render_template, request, redirect
 from models import Project, db, app
+import datetime
 
 
 @app.route('/')
@@ -8,11 +9,11 @@ def index():
     return render_template('index.html', projects=projects)
 
 @app.route('/projects/new', methods=['GET', 'POST'])
-def new_proj():
+def add():
     if request.form:
         new_project = Project(
             title=request.form['title'], 
-            date=request.form['date'], 
+            date=datetime.datetime.strptime(request.form['date'], "%d/%m/%Y"), 
             description=request.form['description'], 
             skills=request.form['skills-list'], 
             link=request.form['github']
@@ -32,7 +33,7 @@ def edit(id):
     project = Project.query.get_or_404(id)
     if request.form:
         project.title=request.form['title']
-        project.date=request.form['date']
+        project.date=datetime.datetime.strptime(request.form['date'], "%d/%m/%Y")
         project.description=request.form['description']
         project.skills=request.form['skills-list']
         project.link=request.form['github']
