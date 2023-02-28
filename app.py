7637a77c9980c9a10ca090319f7cad7d1cd6fd9e
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-02-28 17:29:05 -0600

    V3
    Functioning Mechanically, now to add to the actual substance

--- app.py
+++ app.py
@@ -10,36 +10,39 @@ def index():
 
 @app.route('/projects/new', methods=['GET', 'POST'])
 def add():
+    projects = Project.query.all()
     if request.form:
         new_project = Project(
             title=request.form['title'], 
-            date=datetime.datetime.strptime(request.form['date'], "%d/%m/%Y"), 
-            description=request.form['description'], 
-            skills=request.form['skills-list'], 
+            date=datetime.datetime.strptime(request.form['date'], "%Y-%m"), 
+            description=request.form['desc'], 
+            skills=request.form['skills'], 
             link=request.form['github']
         )
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('projectform.html')
+    return render_template('projectform.html', projects=projects)
 
 @app.route('/projects/<id>')
 def view(id):
+    projects = Project.query.all()
     project = Project.query.get_or_404(id)
-    return render_template('detail.html', project=project)
+    return render_template('detail.html', project=project, projects=projects)
 
 @app.route('/projects/<id>/edit', methods=['GET', 'POST'])
 def edit(id):
+    projects = Project.query.all()
     project = Project.query.get_or_404(id)
     if request.form:
         project.title=request.form['title']
-        project.date=datetime.datetime.strptime(request.form['date'], "%d/%m/%Y")
-        project.description=request.form['description']
-        project.skills=request.form['skills-list']
+        project.date=datetime.datetime.strptime(request.form['date'], "%Y-%m")
+        project.description=request.form['desc']
+        project.skills=request.form['skills']
         project.link=request.form['github']
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('editform.html', project=project)
+        return redirect(url_for('view', id=project.id))
+    return render_template('editform.html', project=project, projects=projects)
 
 @app.route('/projects/<id>/delete')
 def delete(id):
@@ -50,7 +53,12 @@ def delete(id):
 
 @app.route('/projects/about')
 def about():
-    return render_template('about.html')
+    projects = Project.query.all()
+    return render_template('about.html', projects=projects)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
 
 if __name__ == "__main__":
     with app.app_context():
