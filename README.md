4d7152f9925f226319cb99d26de60e5934b59b2d
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-02-28 18:47:23 -0600

    v4
    Can't figure out how to do images

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Lazara's Project Portfolio
 
+--Description--
+A template for making a portfolio with a catalogue of projects that can be added to and removed from.
 
+--Directions to Run--
+Make sure your terminal directory is the folder.
+Create a virtual environment. "python -m venv env"
+Activate the virtual envirnment. ".\name\Scripts\activate"
+Check "requirements.txt" and install all necessary extentions
+Run the program. "python app.py"
 
+--What I Learned--
+I learned a lot about trouble shooting as well. Checking and double checking, scanning for problems and consistancies, etc.
--- app.py
+++ app.py
@@ -12,13 +12,11 @@ def index():
 def add():
     projects = Project.query.all()
     if request.form:
-        new_project = Project(
-            title=request.form['title'], 
+        new_project = Project(title=request.form['title'], 
             date=datetime.datetime.strptime(request.form['date'], "%Y-%m"), 
             description=request.form['desc'], 
             skills=request.form['skills'], 
-            link=request.form['github']
-        )
+            link=request.form['github'])
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))
@@ -35,11 +33,11 @@ def edit(id):
     projects = Project.query.all()
     project = Project.query.get_or_404(id)
     if request.form:
-        project.title=request.form['title']
-        project.date=datetime.datetime.strptime(request.form['date'], "%Y-%m")
-        project.description=request.form['desc']
-        project.skills=request.form['skills']
-        project.link=request.form['github']
+        project.title = request.form['title']
+        project.date = datetime.datetime.strptime(request.form['date'], "%Y-%m")
+        project.description = request.form['desc']
+        project.skills = request.form['skills']
+        project.link = request.form['github']
         db.session.commit()
         return redirect(url_for('view', id=project.id))
     return render_template('editform.html', project=project, projects=projects)
